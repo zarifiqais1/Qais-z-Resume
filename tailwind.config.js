db362916c5e378dfcc428f58d06c3d2c62af523e
Qais Zarifi <zarifiqais1@gmail.com>
2026-05-10 21:06:11 -0700

    fix: sync tailwind and postcss config for stable build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // این خط حیاتی است
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
